175efc0b82f5468f06cc8a9c3a3b00e578d3b604
Michael Dick <b.michael.dick@gmail.com>
2019-09-29 09:24:32 -0400

    Revert "Add back workaround"
    This reverts commit b9593c903a9ef437ba4373ba1986e9424853c163.

--- src/main/java/me/michaeldick/sonosnpr/SonosService.java
+++ src/main/java/me/michaeldick/sonosnpr/SonosService.java
@@ -583,9 +583,9 @@ public class SonosService implements SonosSoap {
 		Media m = ListeningResponseCache.getIfPresent(auth.getUserId()+id);
 		if(m != null) {
 			if(m.getAudioLink() != null) {			
-				//getMediaURIResult.value = m.getAudioLink();
+				getMediaURIResult.value = m.getAudioLink();
 				// Temporary workaround for connection issues
-				getMediaURIResult.value = m.getAudioLink().replace("https://ondemand.npr.org/anon.npr-mp3/","https://sonosnproneproxy.azurewebsites.net/");
+				//getMediaURIResult.value = m.getAudioLink().replace("https://ondemand.npr.org/anon.npr-mp3/","https://sonosnprproxy.azurewebsites.net/");
 				logger.debug("Media URI found: "+getMediaURIResult.value);
 			} else {
 				logger.debug("Item not found");				
