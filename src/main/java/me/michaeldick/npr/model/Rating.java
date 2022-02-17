f472832731d35fc98c1cecd8a12df9bdd41fda0a
Michael Dick <b.michael.dick@gmail.com>
2022-02-16 20:56:39 -0500

    Remove unused imports

--- src/main/java/me/michaeldick/npr/model/Rating.java
+++ src/main/java/me/michaeldick/npr/model/Rating.java
@@ -1,11 +1,7 @@
 package me.michaeldick.npr.model;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
